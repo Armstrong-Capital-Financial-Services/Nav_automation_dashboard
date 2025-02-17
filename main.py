c4cf83d3bad4b8d25c93d355b4ae2ba50a3de1ad
Armstrong-Capital-Financial-Services <divyanshu@armstrong-cap.com>
2025-02-17 11:34:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ import os
 import pandas as pd
 from selenium.webdriver.common.by import By
 
-raw_dict = {"Estee | Gulaq Gear 6": "https://estee.smallcase.com/smallcase/ESTMO_0001",
-            "Estee | Gulaq Gear 5": "https://estee.smallcase.com/smallcase/ESTMO_0002",
-            "Estee | Gulaq Gear 4": "https://estee.smallcase.com/smallcase/ESTMO_0007",
+raw_dict = {"Estee | Gulaq Gear 6 Quant": "https://estee.smallcase.com/smallcase/ESTMO_0001",
+            "Estee | Gulaq Gear 5 Quant": "https://estee.smallcase.com/smallcase/ESTMO_0002",
+            "Estee | Gulaq Gear 4 Quant": "https://estee.smallcase.com/smallcase/ESTMO_0007",
+            "Estee | Gulaq Gear 3 Quant":"https://estee.smallcase.com/smallcase/ESTMO_0003",
             "Finsharpe | Indian Bluechip Leaders": "https://finsharpe.smallcase.com/smallcase/FISHMO_0004",
             "Finsharpe | Large & Mid Cap Diversified": "https://finsharpe.smallcase.com/smallcase/FISHMO_0005",
             "Finsharpe | Mid & Small Cap Focus":"https://finsharpe.smallcase.com/smallcase/FISHMO_0008",
